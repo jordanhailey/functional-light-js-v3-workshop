6a9443986fa8f9ee61fc8ad46668e8d0bf3b0252
jordanhailey <jordan.m.hailey@gmail.com>
2021-03-09 12:09:02 -0800

    adding wrapper function to remove function impurity

--- impurity/ex.js
+++ impurity/ex.js
@@ -8,14 +8,20 @@ var students = [
 	{ id: 491, name: "Ally" }
 ];
 
-function sortStudentsByName() {
-	// Don't modify this function
-	students.sort(function byName(s1,s2){
-		if (s1.name < s2.name) return -1;
-		else if (s1.name > s2.name) return 1;
-		else return 0;
-	});
-	return students;
+// modify/move this function
+function getStudentsByName(s) { 
+	let students = s.slice();
+	function sortStudentsByName() {
+		// Don't modify this function
+		students.sort(function byName(s1,s2){
+			if (s1.name < s2.name) return -1;
+			else if (s1.name > s2.name) return 1;
+			else return 0;
+		});
+		return students;
+	}
+	sortStudentsByName();
+	return students; 
 }
 
 function sortStudentsByID() {
@@ -27,9 +33,6 @@ function sortStudentsByID() {
 }
 
 // *************************************
-
-// modify/move this function
-function getStudentsByName() { return students; }
 
 // modify/move this function
 function getStudentsByID() { return students; }
